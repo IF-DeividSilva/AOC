ddcfbbe4b874015f75dd5b92476010f28171fbb8
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-04-09 15:31:20 -0300

    Create ex5.c

---
+++ Ex_fixacao1/ex5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+       int i = 0;
+    int j = 10;
+    int k = 0;
+
+    while (i < j){
+
+        i++;
+        k +=10;
+
+    }
+
+
+    printf("%i\n", i);
+    printf("%i", k);
+    return 0;
+}
