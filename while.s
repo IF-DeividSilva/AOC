3b82d43942ca81c904f0b3702a57bf71fd125fff
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-04-09 15:31:35 -0300

    Update while.s

--- while.s
+++ while.s
@@ -7,15 +7,15 @@ main:
 li $t0,0
 
 ualy:
-slti $s1, $t0, 10
-beq $s1, $zero, goto
-addi $t0, $t0,1
-j ualy
+    slti $s1, $t0, 10
+    beq $s1, $zero, goto
+    addi $t0, $t0,1
+    j ualy
 
 goto:
-li $v0, 1
-add $a0, $zero, $t5
-syscall
+    li $v0, 1
+    add $a0, $zero, $t5
+    syscall
 
-li $v0, 10
-syscall
+    li $v0, 10
+    syscall
